22dcfcf944e14aff554ee7478b435c3ab179a257
Itamar Holtzman <itamar.holtman@gmail.com>
2025-10-06 16:06:42 +0300

    Update Next.js config to ignore ESLint and TypeScript errors

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,18 @@
-import type { NextConfig } from 'next';
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+import './src/env.js';
 
-const nextConfig: NextConfig = {};
+/** @type {import("next").NextConfig} */
+const config = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 
-export default nextConfig;
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
+
+export default config;
