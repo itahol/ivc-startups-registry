0967bc134bdbb1b09651c6da8fcb629a0eef5652
Itamar Holtzman <itamar.holtman@gmail.com>
2025-11-29 20:07:23 +0200

    Add api-client package with initial setup and config

---
+++ packages/api-client/src/index.ts
@@ -0,0 +1,20 @@
+import type { router } from "api/routes";
+import { hc } from "hono/client";
+
+// create instance to inline type in build
+// https://hono.dev/docs/guides/rpc#compile-your-code-before-using-it-recommended
+export type Client = ReturnType<typeof hc<router>>;
+
+export default (...args: Parameters<typeof hc>): Client => hc<typeof router>(...args);
+
+export type ErrorSchema = {
+  error: {
+    issues: {
+      code: string;
+      path: (string | number)[];
+      message?: string | undefined;
+    }[];
+    name: string;
+  };
+  success: boolean;
+};
